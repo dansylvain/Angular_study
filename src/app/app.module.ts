7a3af8aa34da7b7084fe32b1a9cb501a797c6657
dansylvain <dansylvain84@gmail.com>
2023-12-08 07:19:09 +0100

    session 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'; // comp. must be imported here!!!
+import { ServerComponent } from './server/server.component'; 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+	ServerComponent,
+ ServersComponent // Components must be declared here!!!
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	FormsModule,
+	HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
